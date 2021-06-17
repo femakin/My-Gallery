6c244340a07785406fd1528191e73480e1a83381
Femi Akinyemi <santosfemakin@yahoo.com>
2021-06-17 15:47:37 +0100

    Added Lazy Loading

--- Components/Pages/Home.js
+++ Components/Pages/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { LazyLoadComponent, LazyLoadImage } from 'react-lazy-load-image-component';
 
 import photo1 from "../Images/photo1.png";
 import photo2 from "../Images/photo2.png";
@@ -12,27 +13,27 @@ export default function Home() {
       <div className="container">
         <div className="gallery">
           <figure className="gallery_item gallery__item--1">
-            <img src={photo1} alt="img 1" className="gallery_img" />
+            <LazyLoadImage src={photo1} alt="img 1" className="gallery_img" />
           </figure>
 
           <figure className="gallery_item gallery__item--2">
-            <img src={photo2} alt="img 2" className="gallery_img" />
+            <LazyLoadImage src={photo2} alt="img 2" className="gallery_img" />
           </figure>
 
           <figure className="gallery_item gallery__item--3">
-            <img src={photo3} alt="img 3" className="gallery_img" />
+            <LazyLoadImage src={photo3} alt="img 3" className="gallery_img" />
           </figure>
 
           <figure className="gallery_item gallery__item--4">
-            <img src={photo4} alt="img 4" className="gallery_img" />
+            <LazyLoadImage src={photo4} alt="img 4" className="gallery_img" />
           </figure>
 
           <figure className="gallery_item gallery__item--5">
-            <img src={photo5} alt="img 5" className="gallery_img" />
+            <LazyLoadImage src={photo5} alt="img 5" className="gallery_img" />
           </figure>
 
           <figure className="gallery_item gallery__item--6">
-            <img src={photo6} alt="img 6" className="gallery_img" />
+            <LazyLoadImage src={photo6} alt="img 6" className="gallery_img" />
           </figure>
         </div>
         <footer>
